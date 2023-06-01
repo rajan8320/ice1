62d2ea9ce2b9ae1a280045c5a324047933cee012
Rajan chauhan <Rajan chauhan@10.0.0.29>
2023-05-31 22:57:30 -0700

    ice1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,6 +10,9 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
     
     public static void main(String[] args)
@@ -21,11 +24,41 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            Random number = new Random();
+            
+            c.setValue(number.nextInt(13) + 1);
+            
+            c.setSuit(Card.SUITS[number.nextInt(4)]);
+            
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+           Scanner a = new Scanner(System.in);
+           System.out.print("Enter the digit of your card from 1 to 13: ");
+           int userValue = a.nextInt();
+           System.out.print("Enter the number of suit from 0 to 3: ");
+           int userSuit = a.nextInt();
+
+           Card yourcard = new Card();
+           yourcard.setValue(userValue);
+           yourcard.setSuit(Card.SUITS[userSuit]);
+
+           boolean found = false;
+           for (int i = 0; i < magicHand.length; i++) {
+            if (yourcard.equals(magicHand[i])) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! Your card matches a card in the magic hand!");
+        } else {
+            System.out.println("Sorry! Your card does not matches in the magic hand.");
+        }
     }
     
 }
